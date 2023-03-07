a22106a92849af38bd619e86edaa67bb059fe9d0
Bruno Almeida <brunoalmeidasantos93@gmail.com>
2023-03-07 12:33:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,37 @@
+<h1 align="center"> Ignite Feed </h1>
+
+<p align="center">
+  <a href="#-tecnologias">Tecnologias</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-projeto">Projeto</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#-layout">Layout</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#memo-licença">Licença</a>
+</p>
+
+<p align="center">
+  <img alt="License" src="https://img.shields.io/static/v1?label=license&message=MIT&color=49AA26&labelColor=000000">
+</p>
+
+<br>
+
+<p align="center">
+  <img alt="projeto Ignite" src=".github/ignite-feed.png" width="100%">
+</p>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- Vite
+- ReactJS
+- HTML e CSS
+- TypeScript
+- Git e Github
+- Figma
+
+## 💻 Projeto
+
+Ignite feed é um projeto simples que simula o feed das redes sociais.
+
+## 🔖 Layout
+
+Você pode visualizar o layout do projeto através [DESSE LINK](https://www.figma.com/file/0n0zDN7zbzhRbaEO74Xesx/ToDo-List/duplicate). É necessário ter conta no [Figma](https://figma.com) para acessá-lo.
---
+++ src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
+import { Avatar } from './Avatar';
+import styles from './Comment.module.css';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({ content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0)
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount(likeCount + 1)
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/brunoalmeiida.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Bruno Almeida</strong>
+
+                            <time title='29 de Janeiro às 14:15' dateTime='2023-01-29 14:15:50'>Cerca de 2h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
